b99e21ac07359555ea1e663e84cb1fab3e0c74bd
shubhsd <shubhamsd53@gmail.com>
2020-12-03 12:33:00 +0530

    app.js view changed

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
-import React, {useEffect} from 'react';
-import { SafeAreaView, StyleSheet, StatusBar, Button, Alert } from 'react-native';
+import React, { useEffect } from 'react';
+import { SafeAreaView, StyleSheet, StatusBar, Text, Alert, View } from 'react-native';
 import messaging from '@react-native-firebase/messaging';
 // import firebase from "react-native-firebase";
 
@@ -30,10 +30,10 @@ const App = () => {
   getFcmToken = async () => {
     const fcmToken = await messaging().getToken();
     if (fcmToken) {
-     console.log(fcmToken);
-     console.log("Your Firebase Token is:", fcmToken);
+      console.log(fcmToken);
+      console.log("Your Firebase Token is:", fcmToken);
     } else {
-     console.log("Failed", "No token received");
+      console.log("Failed", "No token received");
     }
   };
 
@@ -41,10 +41,17 @@ const App = () => {
     <>
       <StatusBar barStyle="default" />
       <SafeAreaView style={styles.container}>
-        <Button
-          title="Press me"
-          onPress={() => Alert.alert('Get notification')}
-        />
+        <View style={styles.headingView}> 
+          <Text style={styles.headingText}>
+            Push notifications for Android
+          </Text>
+        </View>
+        <View style={styles.infoView}>
+          <Text>1. Copy fcm token from console of all the devices you want to send notification to.</Text>
+          <Text>2. Paste the fcm token in the firebase cloud messaging section and send the notification from there.</Text>
+          <Text>3. For testing purpose you can open same project in physical device and emulator and keep one application in foreground and another in background.</Text>
+          <Text>4. You will see your push notification here.</Text>
+        </View>
       </SafeAreaView>
     </>
   );
@@ -54,7 +61,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: 'center'
+    alignItems: 'center',
+    padding:20
+  },
+  headingView: {
+    flex:2,
+    flexDirection:'row',
+    justifyContent:'center'
+  },
+  headingText: {
+    fontSize:20,
+    color:'red'
+  },
+  infoView: {
+    flex:4,
+    flexDirection:'column',
+    justifyContent:'space-evenly',
+    alignItems:'flex-start',
+    marginTop:10
   }
 });
 
